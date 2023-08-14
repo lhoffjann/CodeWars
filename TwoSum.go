bf42cecae58f35651d78ae5a016214761a08c5d7
Lennart <hoff.lenn@gmail.com>
2023-08-14 21:02:59 +0200

    add solution for TwoSum

---
+++ TwoSum.go
@@ -0,0 +1,13 @@
+package main
+
+func TwoSum(numbers []int, target int) [2]int {
+	for i, v := range numbers {
+		for j, vj := range numbers {
+			if i != j && (v+vj) == target {
+				return [2]int{i,j}	
+			}
+		}
+		
+	}
+    return [2]int{}
+}
